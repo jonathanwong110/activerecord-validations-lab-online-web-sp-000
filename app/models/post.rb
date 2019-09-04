b41d635259965bfd1b8bc04bce00bdc3048e7502
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-04 18:11:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   validates :content, :length => { :minimum => 250 }
   validates :summary, :length => { :maximum => 250 }
   validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
-  validate :clickbait
+  validates :clickbait
   
   def clickbait
     if !title.nil? && (!title.include?("Won't Believe" || "Secret" || "Top" || "Guess")) 
