1ec33509bcd54978d0441ffe9bc404454d4059be
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-04 19:02:50 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,17 +21,3 @@ class Post < ActiveRecord::Base
 
 end
 
-validate :is_clickbait?
-
-  CLICKBAIT_PATTERNS = [
-    /Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i
-  ]
-  
-  def is_clickbait?
-    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
-      errors.add(:title, "must be clickbait")
-    end
-  end
