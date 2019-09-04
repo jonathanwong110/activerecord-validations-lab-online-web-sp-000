bcf13195f7def5555e62188373c62c860f788eef
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-04 17:14:45 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, :length => { :minimum => 10 }
   validates :summary, :length => { :maximum => 250 }
-  validates :category, :inclusion => {in: (fiction)}
+  validates :category, :inclusion => {in: ("fiction")}
 end
