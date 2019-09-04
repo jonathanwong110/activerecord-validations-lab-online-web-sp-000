3221d38737ff8e889758024ff369d87a3e5f9adc
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-04 17:04:18 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates(:content, { :length => { :in => 10...2000 } })
+  validates(:content, { :length => { :minimum => 10 } })
 end
