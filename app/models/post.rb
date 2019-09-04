c62525f8c292e66e316b91b1ef88d8b0f266faac
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-04 17:44:09 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-  validates :title, :presence => true, inclusion: { in: %w("Won't Believe", "Secret", "Top [number]", or "Guess")}
+  validates :title, :presence => true
   validates :content, :length => { :minimum => 10 }
   validates :summary, :length => { :maximum => 250 }
   validates :category, inclusion: { in: %w(Fiction Non-fiction)}
+  validates :clickbait
 end
