c60c46dad0739021b55215c7cb0c0cbe4918cb2a
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-04 18:28:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   validate :clickbait
   
   def clickbait
-    if !title.nil? && (!title.include?("True Facts")) || (!title.include?(("Won't Believe") || (!title.include?(("Top") || (!title.include?(("Secret")
+    if !title.nil? && (!title.include?("True Facts")) || (!title.include?(("Won't Believe")) || (!title.include?(("Top")) || (!title.include?(("Secret"))
       return false
     end
   end
